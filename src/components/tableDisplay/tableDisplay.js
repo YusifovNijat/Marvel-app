4c69f308417d29c5637079f13a7aa56c3b9cc04e
Nijat <nijatyusifovv@gmail.com>
2023-06-20 00:27:40 +0400

    new commit

--- src/components/tableDisplay/tableDisplay.js
+++ src/components/tableDisplay/tableDisplay.js
@@ -9,11 +9,11 @@ const TableDisplay = ({items, currentTable, setCurrentTable, info, description, 
   const location = useLocation();
   return (
     <div className='table-display'>
-        <Link to='/'><h1 onClick={()=>setCurrentTable(false)}>MARVEL</h1></Link>
+        <Link to='/movieApp'><h1 onClick={()=>setCurrentTable(false)}>MARVEL</h1></Link>
         {currentTable && (location.pathname === `/${itemId}`) ? <> {info} </> 
         : (location.pathname === `/${itemId}/description`) ? <><Breadcrumb itemId={itemId}/>{description}</> 
         : (location.pathname === `/${itemId}/link`) ? <><Breadcrumb itemId={itemId}/> {link}</> 
-        : <Routes><Route exact path='/' element={<MainInfo items={items}/>}/></Routes>}
+        : <Routes><Route exact path='/movieApp' element={<MainInfo items={items}/>}/></Routes>}
     </div>
   )
 }
